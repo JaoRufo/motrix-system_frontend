dd5c571ec88e95bd2550e7fb498ffa0716cb0c6a
Joao Rufo <joaovictorrufopereira44@gmail.com>
2026-02-26 11:19:19 -0300

    Initial commit - estrutura base Quasar

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,14 +2,36 @@ const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      {
+        path: '',
+        redirect: '/ordens',
+      },
+      {
+        path: '/ordens',
+        component: () => import('pages/ordens/OrdemConsulta.vue'),
+      },
+      {
+        path: '/ordens/nova',
+        component: () => import('pages/ordens/OrdemNova.vue'),
+      },
+      {
+        path: '/clientes/cadastro',
+        component: () => import('pages/clientes/ClientesCadastro.vue'),
+      },
+      {
+        path: '/clientes/consulta',
+        component: () => import('pages/clientes/ClientesConsulta.vue'),
+      },
+      {
+        path: '/configuracoes',
+        component: () => import('pages/ConfiguracoesPage.vue'),
+      },
+    ],
   },
-
-  // Always leave this as last one,
-  // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
+    path: '/login',
+    component: () => import('pages/LoginPage.vue'),
   },
 ]
 
